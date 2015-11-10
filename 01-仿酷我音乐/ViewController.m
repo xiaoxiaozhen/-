557670a7bf81c8b12c34dae912459e8b7b3f9711
刀哥 <liufan321@gmail.com>
2015-11-11 01:39:45 +0800

    基本旋转

--- 01-仿酷我音乐/ViewController.m
+++ 01-仿酷我音乐/ViewController.m
@@ -9,19 +9,43 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UIView *demoView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor orangeColor];
+
+    self.demoView = [[UIView alloc] init];
+    self.demoView.backgroundColor = [UIColor whiteColor];
+    
+    // 设置定位点，注意：一定要设置完定位点之后，再设置 frame
+    self.demoView.layer.anchorPoint = CGPointMake(0.5, 1.0);
+    // 设置视图位置
+    self.demoView.frame = self.view.bounds;
+    
+    [self.view addSubview:self.demoView];
+    
+    // 添加手势识别
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesture:)];
+    [self.demoView addGestureRecognizer:pan];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/// 处理拖动手势
+- (void)panGesture:(UIPanGestureRecognizer *)recognizer {
+    
+    CGPoint point = [recognizer translationInView:self.demoView];
+    
+    NSLog(@"%@", NSStringFromCGPoint(point));
+
+    // 倾斜视图
+    CGFloat angle = point.x / self.view.bounds.size.width * M_PI_2;
+    self.demoView.transform = CGAffineTransformRotate(self.demoView.transform, angle);
+    
+    [recognizer setTranslation:CGPointZero inView:self.demoView];
 }
 
 @end
