7be8afc63634563f26620236a9f94f459b08ea0c
xiaoxiaoT <yuanxiaozhen@mfalcons.com>
2016-02-18 16:55:23 +0800

    2016.2.18

--- 仿酷我音乐/ViewController.m
+++ 仿酷我音乐/ViewController.m
@@ -40,9 +40,6 @@
 //    
 //    [self.view addSubview:self.demoView];
 //
-//    // 添加手势识别
-//    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesture:)];
-//    [self.demoView addGestureRecognizer:pan];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
@@ -73,27 +70,5 @@
  a / d 是处理缩放比例的
  a / b / c / d 共同处理旋转角度的
  */
-- (void)panGesture:(UIPanGestureRecognizer *)recognizer {
-    
-    CGPoint point = [recognizer translationInView:self.demoView];
-    
-    CGAffineTransform transform = self.demoView.transform;
-
-    CGFloat viewAngle = atan2(transform.b, transform.a);
-    
-    CGFloat dx = point.x * cos(viewAngle);
-    CGFloat dy = point.y * sin(viewAngle);
-    
-    CGFloat angle = (dx + dy) / self.view.bounds.size.width;
-
-    // 倾斜视图
-    transform = CGAffineTransformRotate(transform, angle);
-    // 平移视图
-    transform.tx += 2 * (dx + dy);
-    
-    self.demoView.transform = transform;
-    
-    [recognizer setTranslation:CGPointZero inView:self.demoView];
-}
 
 @end
